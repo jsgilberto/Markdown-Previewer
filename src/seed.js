41a7f9a3d404e6c7978a607a669e9bd55452ee74
Jesus <alv.mtz94@gmail.com>
2018-09-22 12:02:11 +0200

    added seed file

---
+++ src/seed.js
@@ -0,0 +1,37 @@
+const defaultText = `# hello, This is Markdown Live Preview
+
+----
+## what is Markdown?
+see [Wikipedia](http://en.wikipedia.org/wiki/Markdown)
+
+> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".
+
+----
+## usage
+1. Write markdown text in this textarea.
+2. Click 'HTML Preview' button.
+
+----
+## markdown quick reference
+# headers
+
+*emphasis*
+
+**strong**
+
+* list
+
+> block quote
+
+    code
+[links](http://wikipedia.org)
+
+----
+## changelog
+* 21-September-2018 added styles
+
+----
+## thanks
+* [marked.js](https://github.com/markedjs/marked)`;
+
+export default defaultText;
